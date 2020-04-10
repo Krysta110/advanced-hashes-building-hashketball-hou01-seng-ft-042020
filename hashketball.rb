61c63cde7bc391bb1481d141af060cb2ae606a11
Krysta Wolkowiecki <krystawolkowiecki@outlook.com>
2020-04-10 04:39:31 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,9 +1,13 @@
 # Write your code here!
 game_hash = {   #main hash
-  :home => {
-
+  :home => {    #key of main hash 
+    :team_name => "Brooklyn Nets"  #key of home - holds a String
+    :colors => ["Black", "White"]  #key of home - holds an array
+    :players => {  #key of home - holds a hash 
+      
+    }
   }
-  :away => {
+  :away => { #key of main hash 
     
   }
 }
