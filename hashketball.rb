54359da6e281a88f387f70387cbe9144562ce003
Krysta Wolkowiecki <krystawolkowiecki@outlook.com>
2020-04-10 05:04:48 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -91,14 +91,14 @@ game_hash = {   #main hash
       },
       {    #array location 2 - holds players stats - AWAY TEAM
         :player_name => "DeSagna Diop",
-        :number => 4,
-        :shoe => 18,
-        :points => 10,
-        :rebounds => 1,
-        :assists => 2,
-        :steals => 2,
-        :blocks => 7,
-        :slam_dunks => 2
+        :number => 2,
+        :shoe => 14,
+        :points => 24,
+        :rebounds => 12,
+        :assists => 12,
+        :steals => 4,
+        :blocks => 5,
+        :slam_dunks => 5
       },
 
     ]
