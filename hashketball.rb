16b4ba7e1e56d8b7b4cbfac5fec9806f5e67d49b
Krysta Wolkowiecki <krystawolkowiecki@outlook.com>
2020-04-10 04:48:10 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,23 +1,41 @@
 # Write your code here!
 game_hash = {   #main hash
   :home => {    #key of main hash
-    :team_name => "Brooklyn Nets"  #key of home - holds a String
-    :colors => ["Black", "White"]  #key of home - holds an array
+    :team_name => "Brooklyn Nets",  #key of home - holds a String
+    :colors => ["Black", "White"],  #key of home - holds an array
     :players => [  #key of home - holds an array if hashes
-      "Alan Anderson" => {    #array location 0 - holds players stats
-        :number =>
-        :shoe =>
-        :points =>
-        :rebounds =>
-        :assists =>
-        :steals =>
-        :blocks =>
-        :slam_dunks =>
+      "Alan Anderson" => {    #array location 0 - holds players stats - HOME TEAM
+        :number => "0",
+        :shoe => "16",
+        :points => 22,
+        :rebounds => 12,
+        :assists => 12,
+        :steals => 3,
+        :blocks => 1,
+        :slam_dunks => 1
       },
+      "Reggie Evans" => { #array location 1 - holds players stats - HOME TEAM
+        :number => "30",
+        :shoe => "14",
+        :points => 12,
+        :rebounds => 12,
+        :assists => 12,
+        :steals => 12,
+        :blocks => 12,
+        :slam_dunks => 7
+      },
+      "Brook Lopez" => { #array location 2 - holds players stats - HOME TEAM
 
+      },
+      "Mason Plumlee" => { #arry location 3 - holds players stats - HOME TEAM
 
-  ]
-  }
+      },
+      "Jason Terry" => {
+        
+      }
+
+    ]
+  },
   :away => { #key of main hash
 
   }
