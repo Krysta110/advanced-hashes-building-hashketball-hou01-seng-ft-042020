3e453bec12ce3cea5c79bd68db156d9ed8e102b6
Krysta Wolkowiecki <krystawolkowiecki@outlook.com>
2020-04-10 04:41:29 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -3,9 +3,18 @@ game_hash = {   #main hash
   :home => {    #key of main hash
     :team_name => "Brooklyn Nets"  #key of home - holds a String
     :colors => ["Black", "White"]  #key of home - holds an array
-    :players => {  #key of home - holds a hash
-
-    }
+    :players => [  #key of home - holds an array if hashes
+      "Alan Anderson" => {
+        :number =>
+        :shoe =>
+        :points =>
+        :rebounds =>
+        :assists =>
+        :steals =>
+        :blocks =>
+        :slam_dunks =>
+      }
+  ]
   }
   :away => { #key of main hash
 
